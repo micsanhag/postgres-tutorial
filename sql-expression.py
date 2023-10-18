c83dcbb5f11ab36e3b54cc109809ba58444fbaff
michael <emacen@gmail.com>
2023-10-18 14:05:10 +0200

    update

--- sql-expression.py
+++ sql-expression.py
@@ -10,7 +10,7 @@ meta = MetaData(db)
 # create variable for "Artist" table
 artist_table = Table(
     "Artist", meta,
-    Column("ArtistId", Integer, primary_key=True)
+    Column("ArtistId", Integer, primary_key=True),
     Column("Name", String)
 )
 
