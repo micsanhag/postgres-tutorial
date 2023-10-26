87042edef8a425e9b389bbee01b780c526999649
michael <emacen@gmail.com>
2023-10-26 17:02:47 +0200

    update

---
+++ sql-orm.py
@@ -0,0 +1,15 @@
+from sqlalchemy import (
+    create_engine, column, Float, ForeignKey, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# executing the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
