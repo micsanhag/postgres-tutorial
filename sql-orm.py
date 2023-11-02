3707f44788e3b5e40e3d1815f2e4da3ac1affb58
michael santana <127775871+micsanhag@users.noreply.github.com>
2023-11-02 16:57:40 +0100

    update

--- sql-orm.py
+++ sql-orm.py
@@ -10,8 +10,40 @@ db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
 
+# create a class-based model for the "Artist" table
+class Artist(base):
+    _tablename_ = "Artist"
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(String)
+
+
+# create a class-based model for the "Album" table
+class Album(base):
+    _tablename_ = "Album"
+    AlbumId = Column(Integer, primary_key=True)
+    Title = Column(String)
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
+
+
+# create a class-based model for the "Track" table
+class Track(base):
+    _tablename_ = "Track"
+    TrackId = Column(Integer, primary_key=True)
+    Name = Column(String)
+    AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
+    MediaTypeId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=False)
+    Composer = Column(String)
+    Milliseconds = Column(Integer, primary_key=False)
+    Bytes = Column(Integer, primary_key=False)
+    UnitPrice = Column(Float)
+
+
 # instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
 # opens an actual session by calling the session () subclass defined above
 session = Session()
+
+# creating the database using declarative_base subclass
+base.metadata.create_all(db)
