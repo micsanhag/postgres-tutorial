d57374a8ae859cdcd15d02b5c7b4473e73c717b9
michael santana <127775871+micsanhag@users.noreply.github.com>
2023-11-08 19:01:40 +0100

    update

--- sql-orm.py
+++ sql-orm.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    create_engine, column, Float, ForeignKey, Integer, String
+    create_engine, Column, Float, ForeignKey, Integer, String
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -12,14 +12,14 @@ base = declarative_base()
 
 # create a class-based model for the "Artist" table
 class Artist(base):
-    _tablename_ = "Artist"
+    __tablename__ = "Artist"
     ArtistId = Column(Integer, primary_key=True)
     Name = Column(String)
 
 
 # create a class-based model for the "Album" table
 class Album(base):
-    _tablename_ = "Album"
+    __tablename__ = "Album"
     AlbumId = Column(Integer, primary_key=True)
     Title = Column(String)
     ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
@@ -27,7 +27,7 @@ class Album(base):
 
 # create a class-based model for the "Track" table
 class Track(base):
-    _tablename_ = "Track"
+    __tablename__ = "Track"
     TrackId = Column(Integer, primary_key=True)
     Name = Column(String)
     AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
@@ -42,8 +42,48 @@ class Track(base):
 # instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
-# opens an actual session by calling the session () subclass defined above
+# opens an actual session by calling the Session() subclass defined above
 session = Session()
 
 # creating the database using declarative_base subclass
 base.metadata.create_all(db)
+
+
+# Query 1 - select all records from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.Name)
+
+# Query 3 - select only "Queen" from the "Artist" table
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
+# artist = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+# albums = session.query(Album).filter_by(ArtistId=51)
+# for album in albums:
+#     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(
+        track.TrackId,
+        track.Name,
+        track.AlbumId,
+        track.MediaTypeId,
+        track.GenreId,
+        track.Composer,
+        track.Milliseconds,
+        track.Bytes,
+        track.UnitPrice,
+        sep=" | "
+    )
