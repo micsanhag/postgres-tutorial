148011cb13c7c22efdae6d4481332739f47055c3
michael santana <127775871+micsanhag@users.noreply.github.com>
2023-11-08 19:07:04 +0100

    update

--- sql-orm.py
+++ sql-orm.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    create_engine, Column, Float, ForeignKey, Integer, String
+    create_engine, Column, Integer, String
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,33 +10,15 @@ db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
 
-# create a class-based model for the "Artist" table
-class Artist(base):
-    __tablename__ = "Artist"
-    ArtistId = Column(Integer, primary_key=True)
-    Name = Column(String)
-
-
-# create a class-based model for the "Album" table
-class Album(base):
-    __tablename__ = "Album"
-    AlbumId = Column(Integer, primary_key=True)
-    Title = Column(String)
-    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
-
-
-# create a class-based model for the "Track" table
-class Track(base):
-    __tablename__ = "Track"
-    TrackId = Column(Integer, primary_key=True)
-    Name = Column(String)
-    AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
-    MediaTypeId = Column(Integer, primary_key=False)
-    GenreId = Column(Integer, primary_key=False)
-    Composer = Column(String)
-    Milliseconds = Column(Integer, primary_key=False)
-    Bytes = Column(Integer, primary_key=False)
-    UnitPrice = Column(Float)
+# create a class-based model for the "Programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
 
 
 # instead of connecting to the database directly, we will ask for a session
@@ -49,41 +31,126 @@ session = Session()
 base.metadata.create_all(db)
 
 
-# Query 1 - select all records from the "Artist" table
-# artists = session.query(Artist)
-# for artist in artists:
-#     print(artist.ArtistId, artist.Name, sep=" | ")
+# creating records on our Progammer table
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First Programmer"
+)
 
-# Query 2 - select only the "Name" column from the "Artist" table
-# artists = session.query(Artist)
-# for artist in artists:
-#     print(artist.Name)
+alan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
+)
 
-# Query 3 - select only "Queen" from the "Artist" table
-# artist = session.query(Artist).filter_by(Name="Queen").first()
-# print(artist.ArtistId, artist.Name, sep=" | ")
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL language"
+)
 
-# Query 4 - select only by "ArtistId" #51 from the "Artist" table
-# artist = session.query(Artist).filter_by(ArtistId=51).first()
-# print(artist.ArtistId, artist.Name, sep=" | ")
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
 
-# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
-# albums = session.query(Album).filter_by(ArtistId=51)
-# for album in albums:
-#     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
 
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-tracks = session.query(Track).filter_by(Composer="Queen")
-for track in tracks:
+tim_berners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
+your_name = Programmer(
+    first_name="Your First Name",
+    last_name="Your Last Name",
+    gender="Your Gender",
+    nationality="Your Nationality",
+    famous_for="Celebrate Yourself Here"
+)
+
+# add each instance of our programmers to our session
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+# session.add(your_name)
+
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for = "World President"
+
+# commit our session to the database
+# session.commit()
+
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+# deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
+
+
+# delete multiple/all records
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
+
+
+# query the database to find all Programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
     print(
-        track.TrackId,
-        track.Name,
-        track.AlbumId,
-        track.MediaTypeId,
-        track.GenreId,
-        track.Composer,
-        track.Milliseconds,
-        track.Bytes,
-        track.UnitPrice,
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
         sep=" | "
     )
